b0cfd2cd07ae2ebc1dfd258bdc2a4eb04be7cdbe
Keerthi-Reddy-Baripireddy <144705049+Keerthi-Reddy-Baripireddy@users.noreply.github.com>
2023-09-29 09:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+describe("Testing Bootstrap Modal", () => {
+  it("Opens Bootstrap Modal", () => {
+    cy.visit("http://localhost:3000/main.html"); // Replace with the actual URL of your page
+    cy.get("#open-modal").click(); // Click the button to open the modal
+    // Add assertions or further interactions with the modal here
+  });
+});
